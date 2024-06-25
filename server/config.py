f5329fcb1fc614cad6762c8121ae9f7647cc5fce
tschai-yim <89387771+tschai-yim@users.noreply.github.com>
2024-06-25 14:32:17 +0000

    refactor(server): centralize config

---
+++ server/config.py
@@ -0,0 +1,13 @@
+import os
+from datetime import datetime, timedelta, timezone
+
+DATABASE_HOST = os.environ.get('DATABASE_HOST', 'localhost')
+DATABASE_PORT = os.environ.get('DATABASE_PORT', '5432')
+DATABASE_DATABASE = os.environ.get('DATABASE_DATABASE', 'postgres')
+DATABASE_USERNAME = os.environ.get('DATABASE_USERNAME', 'postgres')
+DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD', 'heliotime')
+DATABASE_URL = f'postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_DATABASE}'
+
+IMPORT_START = datetime.fromisoformat(os.environ['IMPORT_START']).astimezone(timezone.utc) \
+    if 'IMPORT_START' in os.environ else \
+    datetime.now(tz=timezone.utc) - timedelta(days=30)
--- server/migrations/env.py
+++ server/migrations/env.py
@@ -1,27 +1,17 @@
-import os
 from logging.config import fileConfig
 
+from alembic import context
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
-from alembic import context
-
-# Must be in this file because importing relative modules doesn't work here.
-DATABASE_HOST = os.environ.get('DATABASE_HOST', 'localhost')
-DATABASE_PORT = os.environ.get('DATABASE_PORT', '5432')
-DATABASE_DATABASE = os.environ.get('DATABASE_DATABASE', 'postgres')
-DATABASE_USERNAME = os.environ.get('DATABASE_USERNAME', 'postgres')
-DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD', 'heliotime')
+from config import DATABASE_URL
 
 # This is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 fileConfig(config.config_file_name)
 
-config.set_main_option(
-    'sqlalchemy.url',
-    f'postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_DATABASE}'
-)
+config.set_main_option('sqlalchemy.url', DATABASE_URL)
 config.set_main_option('transaction_per_migration', 'true')
 connectable = engine_from_config(
     config.get_section(config.config_ini_section, {}),
