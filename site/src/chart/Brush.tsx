3d3217c22a142d714af79c3ead9b089a57856b54
tschai-yim <89387771+tschai-yim@users.noreply.github.com>
2024-07-18 10:36:47 +0200

    feat(site):  add touch support to brush

--- site/src/chart/Brush.tsx
+++ site/src/chart/Brush.tsx
@@ -51,6 +51,10 @@ class Move {
 
 type Action = Move | StartDraw;
 
+function getClientX(event: MouseEvent | TouchEvent) {
+  return (event instanceof MouseEvent ? event : event.touches[0]).clientX;
+}
+
 export interface BrushProps extends PositionSizeProps {
   view: BrushView;
   onBrush: (view: BrushView) => void;
@@ -59,65 +63,72 @@ export interface BrushProps extends PositionSizeProps {
 export default function Brush({ width, height, top, left, view, onBrush }: BrushProps) {
   const [renderAction, getAction, setAction] = useVolatileState<Action>();
   const [getVolatileView, setVolatileView, syncVolatileView] = useVolatile(view, onBrush);
+  const lastXRef = useRef<number>();
 
   const startAction = useCallback(
-    (newMode: Move | StartDraw) => {
+    (newMode: Move | StartDraw) => (event: React.MouseEvent | React.TouchEvent) => {
+      lastXRef.current = getClientX(event.nativeEvent);
       setAction(newMode);
       syncVolatileView();
     },
     [setAction, syncVolatileView]
   );
 
-  const lastXRef = useRef<number>();
-  useWindowEvent('mousemove', (event: MouseEvent) => {
-    const volatileView = getVolatileView();
-    const interaction = getAction();
+  const processAction = useCallback(
+    (event: MouseEvent | TouchEvent) => {
+      const volatileView = getVolatileView();
+      const interaction = getAction();
 
-    // Track mouse delta (event.movementX is discouraged)
-    const lastX = lastXRef.current ?? event.x;
-    lastXRef.current = event.x;
-    let delta = event.x - lastX;
+      // Track mouse delta (event.movementX is discouraged)
+      const currentX = getClientX(event);
+      const lastX = lastXRef.current ?? currentX;
+      lastXRef.current = currentX;
+      let delta = currentX - lastX;
 
-    // Handle idle
-    if (interaction === undefined) return;
+      // Handle idle
+      if (interaction === undefined) return;
 
-    // Handle start draw action
-    if (interaction === START_DRAW) {
-      if (delta === 0) return;
-      const point = localPoint(event);
-      if (point === null) return;
-      setVolatileView(delta > 0 ? [point.x - delta, point.x] : [point.x, point.x - delta]);
-      setAction({ right: delta > 0, left: delta < 0 });
-      return;
-    }
+      // Handle start draw action
+      if (interaction === START_DRAW) {
+        if (delta === 0) return;
+        const point = localPoint(event);
+        if (point === null) return;
+        setVolatileView(delta > 0 ? [point.x - delta, point.x] : [point.x, point.x - delta]);
+        setAction(delta > 0 ? Move.RIGHT : Move.LEFT);
+        return;
+      }
 
-    // Handle move action
-    if (delta === 0 || volatileView === undefined) return;
-    // Prevent movement beyond border
-    if (delta < 0 && interaction.left) delta = Math.max(-volatileView[0], delta);
-    if (delta > 0 && interaction.right) delta = Math.min(width - volatileView[1], delta);
-    // Create updated view
-    let newView: BrushView = [
-      volatileView[0] + (interaction.left ? delta : 0),
-      volatileView[1] + (interaction.right ? delta : 0),
-    ];
-    // Switch sides if one side was dragged over the other
-    if (newView[0] > newView[1]) {
-      newView = [newView[1], newView[0]];
-      setAction({
-        left: !interaction.left,
-        right: !interaction.right,
-      });
-    }
-    setVolatileView(newView);
-  });
+      // Handle move action
+      if (delta === 0 || volatileView === undefined) return;
+      // Prevent movement beyond border
+      if (delta < 0 && interaction.left) delta = Math.max(-volatileView[0], delta);
+      if (delta > 0 && interaction.right) delta = Math.min(width - volatileView[1], delta);
+      // Create updated view
+      let newView: BrushView = [
+        volatileView[0] + (interaction.left ? delta : 0),
+        volatileView[1] + (interaction.right ? delta : 0),
+      ];
+      // Switch sides if one side was dragged over the other
+      if (newView[0] > newView[1]) {
+        newView = [newView[1], newView[0]];
+        setAction(interaction.left ? Move.RIGHT : Move.LEFT);
+      }
+      setVolatileView(newView);
+    },
+    [getAction, getVolatileView, setAction, setVolatileView, width]
+  );
+  useWindowEvent('mousemove', processAction);
+  useWindowEvent('touchmove', processAction);
 
-  useWindowEvent('mouseup', () => {
+  const endAction = useCallback(() => {
     const interaction = getAction();
     if (interaction === undefined) return;
     if (interaction === START_DRAW) setVolatileView(undefined);
     setAction(undefined);
-  });
+  }, [getAction, setAction, setVolatileView]);
+  useWindowEvent('mouseup', endAction);
+  useWindowEvent('touchcancel', endAction);
+  useWindowEvent('touchend', endAction);
 
   return (
     <svg x={top} y={left} width={width} height={height} className="overflow-visible">
@@ -125,7 +136,8 @@ export default function Brush({ width, height, top, left, view, onBrush }: Brush
         width={width}
         height={height}
         fill="transparent"
-        onMouseDown={() => startAction(START_DRAW)}
+        onMouseDown={startAction(START_DRAW)}
+        onTouchStart={startAction(START_DRAW)}
       />
       {view && (
         <>
@@ -134,19 +146,22 @@ export default function Brush({ width, height, top, left, view, onBrush }: Brush
             width={view[1] - view[0]}
             height={height}
             className="fill-blue-300 opacity-40 cursor-move"
-            onMouseDown={() => startAction(Move.BOTH)}
+            onMouseDown={startAction(Move.BOTH)}
+            onTouchStart={startAction(Move.BOTH)}
           />
           <BrushHandel
             x={view[0]}
             height={height}
             isActive={renderAction instanceof Move && renderAction.left && !renderAction.right}
-            onMouseDown={() => startAction(Move.LEFT)}
+            onMouseDown={startAction(Move.LEFT)}
+            onTouchStart={startAction(Move.LEFT)}
           />
           <BrushHandel
             x={view[1]}
             height={height}
             isActive={renderAction instanceof Move && !renderAction.left && renderAction.right}
-            onMouseDown={() => startAction(Move.RIGHT)}
+            onMouseDown={startAction(Move.RIGHT)}
+            onTouchStart={startAction(Move.RIGHT)}
           />
         </>
       )}
