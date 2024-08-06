141a242618339476b73cd0bf765cb3203db3e8e1
tschai-yim <89387771+tschai-yim@users.noreply.github.com>
2024-08-06 13:47:09 +0200

    feat(site/chart): add pan buttons

--- site/src/chart/flux/FluxChart.tsx
+++ site/src/chart/flux/FluxChart.tsx
@@ -6,7 +6,12 @@ import { NumberRange } from '@/utils/range';
 import { limitView, panView } from '@/utils/panZoom';
 import { useVolatileState } from '@/utils/useVolatile';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAnglesRight, faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import {
+  faAngleLeft,
+  faAngleRight,
+  faAnglesRight,
+  faArrowUpRightFromSquare,
+} from '@fortawesome/free-solid-svg-icons';
 import { getHelioviewerUrl } from '@/api/helioviewer';
 import { FluxMain } from './FluxMain';
 import FluxBrush from './FluxBrush';
@@ -14,6 +19,7 @@ import { View } from './flux';
 
 const FOLLOW_FRONTRUN_PERCENT = 0.1;
 const MIN_VIEW_SIZE_MS = 5 * 60 * 1000;
+const PAN_BUTTON_JUMP = 0.1;
 
 export interface FluxChartProps {
   className?: string;
@@ -29,7 +35,7 @@ export default function FluxChart({ className, selectedTime, onTimeSelect }: Flu
   const [renderRange, getRange, setRange] = useVolatileState<NumberRange>([0, 0]);
   const [renderView, getView, setRawView] = useVolatileState<View>(renderRange);
   const [renderIsFollowing, getIsFollowing, setIsFollowing] = useVolatileState(true);
-  const setView = useCallback(
+  const setUnlimitedView = useCallback(
     (newView: View) => {
       const view = getView();
       const range = getRange();
@@ -40,6 +46,16 @@ export default function FluxChart({ className, selectedTime, onTimeSelect }: Flu
       setRawView(newView);
     },
     [getRange, getView, setIsFollowing, setRawView]
+  );
+  const setView = useCallback(
+    (newView: View) => {
+      const range = getRange();
+      const overflow = Math.min(newView[1] - newView[0], range[1] - range[0]) * 0.9;
+      setUnlimitedView(
+        limitView(newView, [range[0] - overflow, range[1] + overflow], MIN_VIEW_SIZE_MS)
+      );
+    },
+    [getRange, setUnlimitedView]
   );
   const setFollowView = useCallback(
     (viewSize: number) => {
@@ -52,13 +68,13 @@ export default function FluxChart({ className, selectedTime, onTimeSelect }: Flu
     const view = getView();
     setFollowView(view[1] - view[0]);
   }, [getView, setFollowView]);
-  const setLimitedView = useCallback(
-    (newView: View) => {
-      const range = getRange();
-      const overflow = Math.min(newView[1] - newView[0], range[1] - range[0]);
-      setView(limitView(newView, [range[0] - overflow, range[1] + overflow], MIN_VIEW_SIZE_MS));
+
+  const setPanView = useCallback(
+    (relativeDelta: number) => {
+      const view = getView();
+      setView(panView(view, (view[1] - view[0]) * relativeDelta));
     },
-    [getRange, setView]
+    [getView, setView]
   );
 
   // Set default view to 1 day look back
@@ -71,8 +87,8 @@ export default function FluxChart({ className, selectedTime, onTimeSelect }: Flu
     if (dataRange === undefined) return;
     setRange([dataRange[0], Date.now()]);
     // Reapply limits to view incase range got smaller
-    setLimitedView(getView());
-  }, [dataRange, getRange, getView, setLimitedView, setRange]);
+    setView(getView());
+  }, [dataRange, getRange, getView, setView, setRange]);
 
   // Follow live time
   const intervalMs = Math.max(
@@ -86,17 +102,17 @@ export default function FluxChart({ className, selectedTime, onTimeSelect }: Flu
     const interval = setInterval(() => {
       const range = getRange();
       const newEnd = Date.now();
-      if (getIsFollowing()) setView(panView(getView(), newEnd - range[1]));
       setRange([range[0], newEnd]);
+      if (getIsFollowing()) setUnlimitedView(panView(getView(), newEnd - range[1]));
     }, intervalMs);
     return () => clearInterval(interval);
-  }, [getIsFollowing, getRange, getView, intervalMs, setRange, setView]);
+  }, [getIsFollowing, getRange, getView, intervalMs, setRange, setUnlimitedView]);
 
   const viewerUrl = useMemo(() => selectedTime && getHelioviewerUrl(selectedTime), [selectedTime]);
   return (
     <div className={`flex flex-col gap-3 ${className ?? ''}`}>
       <h1 className="sm:hidden text-center">Solar Activity Timeline</h1>
-      <div className="flex px-3">
+      <div className="flex px-3 overflow-hidden">
         <div className="flex-grow basis-0 flex items-center gap-2">
           {(
             [
@@ -119,7 +135,7 @@ export default function FluxChart({ className, selectedTime, onTimeSelect }: Flu
                 {label}
               </button>
             ))}
-          <button type="button" className="btn-tiny" onClick={() => setView(getRange())}>
+          <button type="button" className="btn-tiny" onClick={() => setUnlimitedView(getRange())}>
             All
           </button>
         </div>
@@ -136,13 +152,31 @@ export default function FluxChart({ className, selectedTime, onTimeSelect }: Flu
           >
             <FontAwesomeIcon icon={faAnglesRight} className="aspect-square" />
           </button>
+          <button
+            className="hidden xs:block hmd:block btn-tiny"
+            type="button"
+            onClick={() => setPanView(PAN_BUTTON_JUMP)}
+            aria-label="Pan right"
+          >
+            <FontAwesomeIcon icon={faAngleRight} className="aspect-square" />
+          </button>
+          <button
+            className="hidden xs:block hmd:block btn-tiny "
+            type="button"
+            onClick={() => setPanView(-PAN_BUTTON_JUMP)}
+            aria-label="Pan left"
+          >
+            <FontAwesomeIcon icon={faAngleLeft} className="aspect-square" />
+          </button>
           {viewerUrl && (
             <a
               className="hmd:hidden btn btn-tiny btn-primary text-nowrap"
-              href={viewerUrl} target="_blank" rel="noopener"
+              href={viewerUrl}
+              target="_blank"
+              rel="noopener"
             >
-              <span className='hidden md:inline'>Helioviewer </span>
-              <span className='md:hidden'>HV </span>
+              <span className="hidden md:inline">Helioviewer </span>
+              <span className="md:hidden">HV </span>
               <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
             </a>
           )}
@@ -161,7 +195,7 @@ export default function FluxChart({ className, selectedTime, onTimeSelect }: Flu
             left={mainLeftMargin}
             view={renderView}
             minSizeMs={MIN_VIEW_SIZE_MS}
-            setView={(setter) => setLimitedView(setter(getView()))}
+            setView={(setter) => setView(setter(getView()))}
             onTimeSelect={onTimeSelect}
           />
           <FluxBrush
@@ -171,7 +205,7 @@ export default function FluxChart({ className, selectedTime, onTimeSelect }: Flu
             range={renderRange}
             view={renderView}
             minSizeMs={MIN_VIEW_SIZE_MS}
-            onBrush={setLimitedView}
+            onBrush={setView}
           />
         </svg>
       </div>
