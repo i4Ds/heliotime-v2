e29f149316e89e17b2e5843fd0e685614f6d84aa
tschai-yim <89387771+tschai-yim@users.noreply.github.com>
2024-08-09 14:08:10 +0200

    feat(site/chart): overlap brush axis label with content

--- site/src/chart/flux/FluxBrush.tsx
+++ site/src/chart/flux/FluxBrush.tsx
@@ -11,6 +11,15 @@ import { PositionSizeProps } from '../base';
 import { wattExtent, View, formatTimeOnlyDate } from './flux';
 import Brush from '../Brush';
 
+const CHART_Y_PADDING = 2;
+const BACKDROP_PADDING = 0.15;
+const BACKDROP_BLUR_RADIUS = 0.2;
+
+const backdropFilterOffset = -BACKDROP_PADDING - BACKDROP_BLUR_RADIUS;
+const backdropFilterSize = 1 + 2 * BACKDROP_PADDING + 2 * BACKDROP_BLUR_RADIUS;
+const backdropFloodOffset = -BACKDROP_PADDING;
+const backdropFloodSize = 1 + 2 * BACKDROP_PADDING;
+
 export interface FluxBrushProps extends PositionSizeProps {
   range: NumberRange;
   view: View;
@@ -43,8 +52,7 @@ export default function FluxBrush({
   const wattScale = useMemo(
     () =>
       scaleLog({
-        // Don't go all the way down to prevent overlap with label
-        range: [height - 25, 0],
+        range: [height - 2 * CHART_Y_PADDING, CHART_Y_PADDING],
         domain: wattExtent(data, 0.05),
         clamp: true,
       }),
@@ -57,6 +65,37 @@ export default function FluxBrush({
   }, [timeScale, view, width]);
   return (
     <svg width={width} height={height} y={top} x={left} className="overflow-visible">
+      <defs>
+        <filter
+          id="label-backdrop"
+          x={backdropFilterOffset}
+          y={backdropFilterOffset}
+          width={backdropFilterSize}
+          height={backdropFilterSize}
+          primitiveUnits="objectBoundingBox"
+        >
+          <feFlood
+            x={backdropFloodOffset}
+            y={backdropFloodOffset}
+            width={backdropFloodSize}
+            height={backdropFloodSize}
+            floodColor={colors.bg.DEFAULT}
+            floodOpacity={0.5}
+          />
+          <feGaussianBlur
+            x={backdropFilterOffset}
+            y={backdropFilterOffset}
+            width={backdropFilterSize}
+            height={backdropFilterSize}
+            stdDeviation={BACKDROP_BLUR_RADIUS}
+            result="backdrop"
+          />
+          <feMerge>
+            <feMergeNode in="backdrop" />
+            <feMergeNode in="SourceGraphic" />
+          </feMerge>
+        </filter>
+      </defs>
       <GridColumns scale={timeScale} height={height} numTicks={8} stroke={colors.bg[1]} />
       <LinePath
         curve={curveMonotoneX}
@@ -75,7 +114,12 @@ export default function FluxBrush({
         tickLength={0}
         stroke={colors.text.DEFAULT}
         tickStroke={colors.text.DEFAULT}
-        tickLabelProps={{ fill: colors.text.DEFAULT, ...textSize.xs, ...font.style }}
+        tickLabelProps={{
+          fill: colors.text.DEFAULT,
+          ...textSize.xs,
+          ...font.style,
+          filter: 'url(#label-backdrop)',
+        }}
       />
       <Brush
         width={width}
