77f8aa9cb000ff822f5f91b7c7fd0e7f92a7e21c
tschai-yim <89387771+tschai-yim@users.noreply.github.com>
2025-01-22 17:39:25 +0100

    perf(site): lazy load range controls

--- site/src/components/player/header/ChartHeader.tsx
+++ site/src/components/player/header/ChartHeader.tsx
@@ -7,11 +7,16 @@ import IconButton from './IconButton';
 import { usePlayerSettings } from '../state/settings';
 import { usePanControl } from '../state/pan';
 import { ViewerButton } from './ViewerButton';
-import { RangeButtons, RangeDropdown } from './range';
 
 const DynamicJumpButton = dynamic(() => import('./JumpButton'), { ssr: false });
 const DynamicSettingsButton = dynamic(() => import('./SettingsButton'), { ssr: false });
 const DynamicShareButton = dynamic(() => import('./ShareButton'), { ssr: false });
+const DynamicRangeButtons = dynamic(() => import('./range').then((m) => m.RangeButtons), {
+  ssr: false,
+});
+const DynamicRangeDropdown = dynamic(() => import('./range').then((m) => m.RangeDropdown), {
+  ssr: false,
+});
 
 const CHART_TITLE = 'Solar Activity Timeline';
 
@@ -28,7 +33,7 @@ export default function ChartHeader() {
       <h1 className="block sm:hidden text-center">{CHART_TITLE}</h1>
       <div className="flex overflow-x-auto gap-2">
         <div className="hidden lg:flex hmd:hidden hmd:md:flex flex-grow basis-0 items-center gap-2">
-          <RangeButtons buttonsClassName="btn-tiny" />
+          <DynamicRangeButtons buttonsClassName="btn-tiny" />
         </div>
         <div className="hidden sm:flex overflow-x-auto gap-2">
           <h1 className="text-nowrap select-text">{CHART_TITLE}</h1>
@@ -36,16 +41,14 @@ export default function ChartHeader() {
         </div>
         <div className="flex-grow basis-0 flex items-center justify-center sm:justify-end gap-2">
           <ViewerButton className={`${settings.showPreview ? 'hmd:hidden' : ''} btn-tiny`} />
-          <AboutLink
-            className="text-2xl block sm:hidden"
-          />
+          <AboutLink className="text-2xl block sm:hidden" />
           <DynamicShareButton
             className="btn-tiny"
             data={() => ({ url: globalThis.location.href, title: 'Heliotime' })}
             title="Share view"
           />
           <DynamicSettingsButton className="btn-tiny" />
-          <RangeDropdown className="lg:hidden hmd:md:hidden btn-tiny" />
+          <DynamicRangeDropdown className="lg:hidden hmd:md:hidden btn-tiny" />
           <DynamicJumpButton className="btn-tiny" />
           <IconButton
             icon={faAngleLeft}
